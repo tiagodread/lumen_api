13609a0acea16874206165145f3afe850718a0a1
Tiago Góes <tiago.goes2009@hotmail.com>
2018-11-06 01:01:18 -0200

    Added Readme.md and change database setup

---
+++ README.md
@@ -0,0 +1,7 @@
+#welcome to lumen api documentation
+
+###Install
+
+1. git clone https://github.com/tiagodread/lumen_api.git
+2. cd lumen_api
+3. 
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,9 @@
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432/lumen'
+    DB_USER = 'postgres'
+    DB_PASS = ''
+    DB_PORT = '5432'
+    DB_NAME = 'lumen'
+    DB_HOST = 'localhost'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@%s:%s/%s' % (DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
